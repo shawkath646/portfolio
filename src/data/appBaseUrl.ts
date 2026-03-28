36eccf97cc36f8146dacfc95f92355731785b618
Shawkat Hossain Maruf <shawkath646@gmail.com>
2026-03-29 06:05:13 +0900

    Fix: Base URL configuration

--- src/data/appBaseUrl.ts
+++ src/data/appBaseUrl.ts
@@ -1,4 +1,4 @@
-const rawUrl = process.env.NEXT_PUBLIC_SITE_URL ||
+const rawUrl = process.env.NEXT_PUBLIC_APP_BASE_URL ||
     (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
     "http://localhost:3000";
 
